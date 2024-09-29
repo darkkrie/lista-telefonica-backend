2d8f833100c0db0ae71733bfdf325dab8d95e33a
darkkrie <pedrovitorthd@gmail.com>
2024-09-28 21:15:21 -0300

    criando projeto

---
+++ src/test/java/listatelefonica/com/ListatelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package listatelefonica.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListatelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
